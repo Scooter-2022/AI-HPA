4be2c9433826d43e29b9a96b7f2f19df4ed487cc
bconfiden2 <bconfiden2@naver.com>
2022-07-06 09:18:54 +0900

    반환받은 값으로 스케일링 적용

--- Update/utils.py
+++ Update/utils.py
@@ -42,5 +42,6 @@ def insert_usage(usage_data, usage):
 def predict_replica(usage_data, curr_pods):
     URL = "http://localhost:4000/predict"
     data = {'usage_data':usage_data, 'curr_pods': curr_pods}
-    requests.post(URL, json=data)
-    return
+    response = requests.post(URL, json=data)
+    replicas = int(response.text)
+    return replicas
