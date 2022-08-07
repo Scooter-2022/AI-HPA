7038fdcc9669bc4c715c3034fc4e315d245b52a7
Nayeon Keum <rmaskdus0208@gmail.com>
2022-08-07 19:01:08 +0900

    [feat/#32] ADD: model and prerequisites

--- Predict/model.py
+++ Predict/model.py
@@ -1,11 +1,19 @@
 import time
 import random
+import math
+from tensorflow.keras.models import load_model
+import numpy as np
 
 # fastapi 에서 호출하는 함수
 def predict(usage_data, curr_pods):
     
     # 임의로 설정한 inference 시간
     time.sleep(3)
-    replicas = random.randrange(1, 6)
+    
+    model=load_model('./bi_lstm_model0.h5', compile = False)
 
+    usage_data=np.array(usage_data).reshape(1, 10, 1)
+    yhat=model.predict(usage_data)
+    replicas=math.ceil(yhat[0][0])
+    
     return replicas
